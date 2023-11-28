ee7bcbc34510bf7903360644065f7de8dac1e006
ghalib_24 <muhamadghalib7@gmail.com>
2023-11-28 08:41:24 +0700

    feat: update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,12 +14,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        
+        \App\Models\User::create([
+            'name'=>'resepsionis',
+            'username'=>'admin',
+            'password'=>Hash::make('resepsionis123'),//resepsionis123
+            'role'=>'resepsionis',
+            'email'=>'resepsionis@gmail.com',
+            'email_verified_at'=>now()
+        ]);
+
         \App\Models\User::create([
             'name'=>'admin',
             'username'=>'admin',
-            'password'=>'$2y$10$KHOT48LRGRNe2Sp01JHffOszE5VN8.JN5.SMfCGpFJHg.hwEz.jU6',//admin123
+            'password'=>Hash::make('admin123'),
             'role'=>'admin',
             'email'=>'admin@gmail.com',
             'email_verified_at'=>now()
@@ -27,18 +35,9 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::create([
             'name'=>'tamu',
             'username'=>'tamu',
-            'password'=>'$2y$10$KHOT48LRGRNe2Sp01JHffOszE5VN8.JN5.SMfCGpFJHg.hwEz.jU6',//admin123
+            'password'=>Hash::make('tamu123'),
             'role'=>'tamu',
             'email'=>'tamu@gmail.com',
-            'email_verified_at'=>now()
-        ]);
-
-        \App\Models\User::create([
-            'name'=>'resepsionis',
-            'username'=>'resepsionis',
-            'password'=>'$2y$10$KHOT48LRGRNe2Sp01JHffOszE5VN8.JN5.SMfCGpFJHg.hwEz.jU6',//admin123
-            'role'=>'resepsionis',
-            'email'=>'resepsionis@gmail.com',
             'email_verified_at'=>now()
         ]);
     }
